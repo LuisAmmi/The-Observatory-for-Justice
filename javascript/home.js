e6cf30cbc454e5e6b2d2b92c13ce234bdf07609c
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-01-24 23:38:44 +0100

    cambiare tutti i paths e nomi file

--- javascript/home.js
+++ javascript/home.js
@@ -1,7 +1,11 @@
 containermultimedia
 /* Cambiare stile CSS nella homepage*/
 $(document).ready(function(){
-  $('#1970').click(function(){
-    $('#stylepage').attr('href', '../The Observatory for Justice/css/home70'.css');
+  $('#topolinoicon').click(function(){
+    $('#stylepage').attr('href', 'css/home70.css');
+  });
+
+  $('#dadaicon').click(function(){
+    $('#stylepage').attr('href', 'css/homedada.css');
   });
 });
--- javascript/issue.js
+++ javascript/issue.js
@@ -1,51 +1,10 @@
-/* const videohomepage= document.getElementById('videohomepage');
-
-videohomepage.addEventListener('click', function() {
-  window.scrollTo(0, 0);
-});
-
-window.addEventListener('scroll', function(){
-  let posY = window.scrollY;
-  if posY > 0{
-
-  }
-  else {
-
-  }
-})
-
+/* Cambiare stile CSS*/
 $(document).ready(function(){
-  $("button").click(function(){
-    $("p").hide();
+  $('#dadaicon').click(function(){
+    $('#stylepage').attr('href', 'css/issuedada.css');
   });
-});
-
-
-$(document).ready(function(){
-  $("#videohomepage").mouseover(function(){
-    $(this).click(function(){
-      $(window).scrollY();
-    });
-  });
-});
-
-function scroll_to(id) {
-  $('html,body').animate({
-    scrollTop: $('#'+id).offset().top
-  },'slow');
-}
-/*
-jQuery Scroll to ID
-*/
-/* $(document).ready(function(){
-  $('#specialedition').hover(function(){
-    $(this).hide();
-  }
-}) */
-
-/* Cambiare stile CSS nella homepage*/
-$(document).ready(function(){
-  $('#1970').click(function(){
-    $('#stylepage').attr('href', '../The Observatory for Justice/css/my70issue.css');
+  
+  $('#topolinoicon').click(function(){
+    $('#stylepage').attr('href', 'css/issue70.css');
   });
 });
