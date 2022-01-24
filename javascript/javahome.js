cb41c5632e31bcf922073ec5c872cec0c4b78865
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-01-24 17:52:21 +0100

    FF

---
+++ javascript/javahome.js
@@ -0,0 +1,7 @@
+containermultimedia
+/* Cambiare stile CSS nella homepage*/
+$(document).ready(function(){
+  $('#1970').click(function(){
+    $('#stylepage').attr('href', '../The Observatory for Justice/css/home70'.css');
+  });
+});
