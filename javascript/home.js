c873438b34e0b497e4993fa8ad373a5d072f5e1a
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-01-26 15:53:01 +0100

    css home fumetto

--- javascript/home.js
+++ javascript/home.js
@@ -3,11 +3,14 @@ containermultimedia
 $(document).ready(function(){
   $('#topolinoicon').click(function(){
     $('#stylepage').attr('href', 'css/home70.css');
-        $("#videohomepage").remove();
+    $("#videohomepage").remove();
+    $('#containermultimedia').html('<img id="comicshomepage"  alt="comics about police brutality" src="./imgs/70/Homepage70/collage5.jpg">');
+
   });
 
   $('#dadaicon').click(function(){
     $('#stylepage').attr('href', 'css/homedada.css');
         $("#videohomepage").remove();
+          $('#containermultimedia').html('<img id="collagedada"  alt="collage dada style" src="./Documentation/dada/Home collage/collage homepage-cutout.png">');
   });
 });
