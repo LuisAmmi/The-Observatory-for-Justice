c125846710747904e9cbd016ce141381ce5f620e
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-02-02 19:26:43 +0100

    home dada css

--- javascript/home.js
+++ javascript/home.js
@@ -1,4 +1,4 @@
-containermultimedia
+
 /* Cambiare stile CSS nella homepage*/
 $(document).ready(function(){
   $('#topolinoicon').click(function(){
@@ -12,9 +12,11 @@ $(document).ready(function(){
     $('#stylepage').attr('href', 'css/homedada.css');
         $("#videohomepage").remove();
           $('#containermultimedia').html('<img id="collagedada"  alt="collage dada style" src="./Documentation/dada/Home collage/collage homepage-cutout.png">');
+          $(".project").html('<h2 class="section_heading" id="about">About</h2><hr> <h2 class="section_heading" id="theproject">The project</h2> <p>The Observatory for Justice is a project realized for the examination of the course of Information modelling and web technologies held by Fabio Vitali within the Master Degree in Digital Humanities and Digital Knowledge of the University of Bologna. According to the guideline, the purpose of the website is to display three articles dealing with the same theme/main topic. The scopes of the site are displaying articles through different styles and layouts which should be influenced by real typographic movements, achievable through a cross-analysis of textual metadata. So, let&rsquo;s explore the different styling taste from the past. Thanks to the style timeline, in the Filter section of the navbar, the user can access to this first number of the magazine switching from a very basic and contemporary style to one inspired by the Dada movement of 1920 and to 70’s style, shaped on the famous “Topolino” comic book (“Mickey Mouse” in English). </p> <img id="ingranaggio" src="imgs/dada/ingranaggio.png">');
+          $("#specialedition").html('<p class="question" id="specialtext"><span class="wh">Who</span> do you call <span class="wh">when</span> people <span class="wh">who</span> should protect you are the <span id="abusers">abusers</span>?</p> <img id="hurrahurra" src="imgs/dada/hurrahurra.png">');
+
   });
 });
-
 $(document).ready(function(){
   $("#sources").click(function(){
     $("#panelsource").slideToggle("slow");
