da2bbc493e039db22158987f173f18bb322775ef
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-02-13 23:52:59 +0100

    Disclaimer

--- javascript/issue.js
+++ javascript/issue.js
@@ -17,3 +17,26 @@ $(document).ready(function(){
     $("#end").html('<img id="quofine" src="imgs/70/quofine.png">');
   });
 });
+
+// Disclaimer
+function disclaimer(){
+      var modal = document.getElementById("myModal");
+      // Get the button that opens the modal
+      var btn = document.getElementById("disclaimer");
+      // Get the <span> element that closes the modal
+      var span = document.getElementsByClassName("close")[0];
+      // When the user clicks the button, open the modal
+      btn.onclick = function(){
+        modal.style.display = "block";
+      }
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
+        modal.style.display = "none";
+      }
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+};
