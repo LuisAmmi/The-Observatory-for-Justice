db573b7686b720511f24cb03764b7102eb3424f8
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-03-01 15:15:41 +0100

    4

--- javascript/issue.js
+++ javascript/issue.js
@@ -43,12 +43,27 @@ $(document).ready(function(){
 
 // sidebar
 $(document).ready(function () {
+        $("#sidebar").mCustomScrollbar({
+            theme: "minimal"
+        });
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
+        $('#dismiss, .overlay').on('click', function () {
+            // hide sidebar
+            $('#sidebar').removeClass('active');
+            // hide overlay
+            $('.overlay').removeClass('active');
+        });
+
+        $('#sidebarCollapse').on('click', function () {
+            // open sidebar
+            $('#sidebar').addClass('active');
+            // fade in the overlay
+            $('.overlay').addClass('active');
+            $('.collapse.in').toggleClass('in');
+            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+        });
     });
-
-});
+    
 // Disclaimer
 function disclaimer(){
       var modal = document.getElementById("myModal");
