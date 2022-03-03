2c4474549f2f72de97af524a1ee974b3f8e10264
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-03-03 12:19:25 +0100

    ,

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -1,5 +1,5 @@
 // filter mention elements
-function SectionHighligh{
+function SectionHighligh(section1){
   let section1 = document.getElementsById('MarkCovelscase');
   section1.css({
       "background-color": "#fff"
