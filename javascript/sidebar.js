50ce9fd1b6227341cad573741db7fde75221e4a5
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-03-03 14:23:34 +0100

    Update sidebar.js

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -1,8 +1,10 @@
 
+
   function highlightSection(){
     if (document.getElementById("covel").checked = true)
     {
       $('#MarkCovelscase').css({"background-color": "#fff"});
+      $('article1').scrollTo('#MarkCovelscase');
     }
     else {
       $('#MarkCovelscase').css({"background-color": "none"});
