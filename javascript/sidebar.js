55199bac24823befd67558ee8526a90a4118258b
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-03-03 15:30:02 +0100

    Update sidebar.js

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -5,7 +5,7 @@
           if ($(this).prop("checked") == true)
             {
               $('#MarkCovelscase').css({"background-color": "#fff"});
-              $('#MarkCovelscase').scrollTop();
+              $('#article1').scrollTop('#MarkCovelscase');
             }
           else if ($(this).prop("checked") == false)
            {
