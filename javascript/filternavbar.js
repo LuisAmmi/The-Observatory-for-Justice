aab30ba86ed4e4a6ef163e334dc4443a48818dc6
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-03-04 15:51:46 +0100

    f

--- javascript/filternavbar.js
+++ javascript/filternavbar.js
@@ -1,12 +1,23 @@
-var mydict = {
+// "this" nasce dallo <span onlick="higlight()"></span> messo sul ex. <input type="checkbox" class="form-check-input" active="0" id="covel">
+function highlight(this){
+var dict = {
 "covel": "MarkCovelscase",
 "diaz": "reportonDiazPertini",
 "bolzaneto": "BolzanetoDistrict",
-"outside": "theoutsideworld"
+"outside": "theoutsideworld",
+"next": "whathappenednext"
 };
-var mydict["next"] = "whathappenednext";
+//  : "2-intro", : "2-badapples", : "2-evidencebased", : "updatesandcorrections2", : "references2", : "abstract3",  : "3-intro",  : "3-methods",  : "3-results", : "3-discussion", : "3-conclusions", : "authorandarticleinfo3", : "references3"
+//cos'è this? l'elemento nel complesso? oppure l'id? per prendere solo l'id forse bisogna fare idthis = $this.getId...
 
- //  : "2-intro", : "2-badapples", : "2-evidencebased", : "updatesandcorrections2", : "references2", : "abstract3",  : "3-intro",  : "3-methods",  : "3-results", : "3-discussion", : "3-conclusions", : "authorandarticleinfo3", : "references3"
-
-
-console.log(mydict);
+for (var key in dict) {  // "covel", "diaz", "bolzaneto", "outside", "next"
+  if (key == $(this)) //ex. "covel"
+    var value = dict[$(this)]; //secondo il principio var value = dict[key] "MarkCovelscase", "reportonDiazPertini"...
+    const selectedParagraph = document.querySelectorAll(value); //associa l'id del paragrafo ("value") al paragrafo, selezionato = tutto salvato in una variabile
+      if ($(this).prop("checked") == true)
+         selectedParagraph.style.backgroundColor = "#fff";
+      else if ($(this).prop("checked") == false)
+        selectedParagraph.style.backgroundColor = "transparent";
+  // do something with "key" and "value" variables
+ }
+}
