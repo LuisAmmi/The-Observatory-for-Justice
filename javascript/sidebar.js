784a96274a47ef7f2baaea3a3fec185ef5d5f787
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-03-04 11:31:28 +0100

    n

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -1,4 +1,42 @@
 
+  function highlightSection("covel"){
+    var firstsection = document.getElementById("covel");
+    if (firstsection.checked = true)
+    {
+      $('#MarkCovelscase').css({"background-color": "#fff"});
+      $('article1').scrollTo('#MarkCovelscase');
+    }
+    else {
+      $('#MarkCovelscase').css({"background-color": "none"});
+    }
+  }
+
+// QUELLA CHE FUNZIONA
+  $('#covel').click(function(){
+        if ($(this).prop("checked") == true)
+          {
+            $('#MarkCovelscase').css({"background-color": "#fff"});
+            $('#article1').scrollTop();
+          }
+        else if ($(this).prop("checked") == false)
+         {
+          $('#MarkCovelscase').css({"background-color": "transparent"});
+         }
+    });
+  // FINE
+
+      $('#covel').click(function(){
+            if ($(this).checked = true)
+              {
+                $('#MarkCovelscase').css({"background-color": "#fff"});
+                $('#article1').scrollTop('');
+              }
+            else if($(this).prop("checked") == false)
+             {
+              $('#MarkCovelscase').css({"background-color": "transparent"});
+             }
+        });
+
   $('#covel').click(function(){
         const nodeList = document.querySelectorAll("#MarkCovelscase");
         for (let i = 0; i < nodeList.length; i++)
@@ -9,4 +47,14 @@
         else if ($(this).prop("checked") == false)
           {
             nodeList[i].style.backgroundColor = "transparent";
-          }
+          }  $('#covel').click(function(){
+                  const nodeList = document.querySelectorAll("#MarkCovelscase");
+                  for (let i = 0; i < nodeList.length; i++)
+                  if ($(this).prop("checked") == true)
+                    {
+                    nodeList[i].style.backgroundColor = "#fff";
+                    }
+                  else if ($(this).prop("checked") == false)
+                    {
+                      nodeList[i].style.backgroundColor = "transparent";
+                    }
---
+++ resultofPythonfunction.js
@@ -0,0 +1,28 @@
+FOR EACH CATEGORY: ALL THE 'ABOUT'S AND THE NUM OF TIMES THEY APPEAR
+company [{'Independent Media Center': 2}, {'Getty': 1}, {'PacificPress': 1}, {'PubMed': 4}, {'Article': 12}, {'GoogleScholar': 118}, {'Web of Science': 3}, {'PsycINFO': 1}, {'JSTOR': 1}, {'Criminal Justice Abstracts': 1}, {'Sociological Abstracts': 1}, {'National Criminal Justice Reference Service': 1}, {'Crossref': 57}, {'Medline': 50}]
+company newspaper [{'La Repubblica': 1}, {'Daily Mail': 1}, {'Nature': 2}, {'The New York Times': 1}]
+concept [{'fascism': 8}, {'torture': 3}, {'neo-fascism': 1}, {'police brutality': 2}, {'racial bias': 5}, {'racism': 4}, {'police accountability': 4}, {'use of force': 6}, {'police violence': 1}, {'misconduct': 7}, {'abuse of power': 1}]
+disorder [{'mental disorder': 62}, {'dementia': 1}, {'intellectual disability': 1}, {'bipolar disorder': 3}, {'schizophrenia': 1}]
+ethnic_group [{'Black people': 11}, {'White people': 5}, {'Latino people': 1}, {'Black man': 1}]
+event [{'27th G8 summit': 2}, {'raid on Armando Diaz': 5}, {'killing of Eric Garner': 1}, {'killing of George Floyd': 2}]
+institution Police-Department [{'Bolzaneto Department': 5}, {'New York City Police Department': 1}, {'Oklahoma City Police Department': 2}, {'Metropolitan Police Department of the District of Columbia': 1}, {'Chicago Police Department': 1}]
+institution University [{'University of Nebraska Omaha': 1}, {'Texas A&M University': 1}, {'University of South Carolina': 1}, {'George Mason University': 1}, {'University of Illinois at Urbana–Champaign': 1}, {'Saint Mary’s University': 1}]
+organization [{'Reuters': 1}]
+person [{'Nick Davies': 2}, {'Sergei Karpukhin': 1}, {'Mark Covell': 20}, {'Enrico Zucca': 9}, {'Michael Gieser': 4}, {'Melanie Jonasch': 1}, {'Katherina Ottoway': 1}, {'Dan McQuillan': 4}, {'Norman Blair': 3}, {'Sam Buchanan': 1}, {'Nicola Doherty': 2}, {'Richard Moth': 2}, {'Daniel Albrecht': 2}, {'Karl Boro': 1}, {'Jaraslaw Engel': 1}, {'Lena Zuhlke': 4}, {'Niels Martensen': 3}, {'Mussolini': 3}, {'Pinochet': 2}, {'Mohammed Tabach': 1}, {'Stefan Bauer': 1}, {'Marco Bistacchia': 1}, {'Ester Percivati': 2}, {'David Larroquelle': 1}, {'Tony Blair': 1}, {'Gianfranco Fini': 1}, {'Silvio Berlusconi': 1}, {'Massimo Pastore': 2}, {'Lynne Peeples': 1}, {'ElijahNouvelage': 1}, {'Derek Chauvin': 5}, {'George Floyd': 7}, {'Michael Brown': 1}, {'Eric Garner': 1}, {'Justin Nix': 2}, {'Tracey Meares': 1}, {'Mark Hoekstra': 2}, {'Seth Stoughton': 4}, {'SteveSanchez': 1}, {'Lawrence Sherman': 5}, {'Tamir Rice': 1}, {'Donald Trump': 1}, {'Robin Engel': 5}, {'Laurie Robinson': 1}, {'Megan Morgan': 1}, {'Thomas O’Brien': 1}, {'Ahmaud Arbery': 1}, {'James D. Livingston': 3}, {'Zachary MacMillan': 1}, {'Karen Chu': 1}, {'Michelle Pritchard': 1}]
+place [{'Genoa': 3}, {'Berlin': 2}, {'Stansted': 1}, {'New Zealand': 1}, {'London': 1}, {'Poland': 1}, {'San Martino hospital': 2}, {'European Union': 1}, {'Minneapolis': 2}, {'United States': 13}, {'Ferguson': 1}, {'Missouri': 1}, {'New York City': 4}, {'California': 1}, {'New Haven': 1}, {'Connecticut': 1}, {'College Station': 1}, {'Chicago': 1}, {'Illinois': 1}, {'Columbia': 1}, {'Cambridge': 1}, {'United Kingdom': 3}, {'Cleveland': 1}, {'Ohio': 2}, {'Washington DC': 1}, {'Cincinnati': 1}, {'Fairfax': 1}, {'Virginia': 1}, {'Nevada': 1}, {'Georgia': 1}, {'Canada': 3}]
+type-of-aggression [{'beating': 15}, {'kicking': 7}, {'dragging': 5}, {'sexual misconduct': 1}, {'verbal violence': 7}, {'shooting': 9}, {'chokehold': 2}]
+
+NUMBER OF MENTIONS FOR EACH CATEGORY
+Police-Department 10
+University 6
+company 258
+concept 42
+disorder 68
+ethnic_group 18
+event 10
+institution 16
+newspaper 5
+organization 1
+person 125
+place 56
+type-of-aggression 46
