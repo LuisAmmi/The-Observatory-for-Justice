b2d6c0f835a803d8ea6142cda48815583319205f
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-03-04 12:11:52 +0100

    b

--- javascript/filternavbar.js
+++ javascript/filternavbar.js
@@ -1,2 +1,12 @@
+var mydict = {
+"covel": "MarkCovelscase",
+"diaz": "reportonDiazPertini",
+"bolzaneto": "BolzanetoDistrict",
+"outside": "theoutsideworld"
+};
+var mydict["next"] = "whathappenednext";
 
-console.log("hello");
+ //  : "2-intro", : "2-badapples", : "2-evidencebased", : "updatesandcorrections2", : "references2", : "abstract3",  : "3-intro",  : "3-methods",  : "3-results", : "3-discussion", : "3-conclusions", : "authorandarticleinfo3", : "references3"
+
+
+console.log(mydict);
