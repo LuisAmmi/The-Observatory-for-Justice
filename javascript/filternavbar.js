66c161c31ca3131578d117110d619235be8892ea
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-03-04 15:57:32 +0100

    Update filternavbar.js

--- javascript/filternavbar.js
+++ javascript/filternavbar.js
@@ -1,5 +1,5 @@
 // "this" nasce dallo <span onlick="higlight()"></span> messo sul ex. <input type="checkbox" class="form-check-input" active="0" id="covel">
-function highlight(this){
+function highlight(x){
 var dict = {
 "covel": "MarkCovelscase",
 "diaz": "reportonDiazPertini",
@@ -11,12 +11,12 @@ var dict = {
 //cos'è this? l'elemento nel complesso? oppure l'id? per prendere solo l'id forse bisogna fare idthis = $this.getId...
 
 for (var key in dict) {  // "covel", "diaz", "bolzaneto", "outside", "next"
-  if (key == $(this)) //ex. "covel"
-    var value = dict[$(this)]; //secondo il principio var value = dict[key] "MarkCovelscase", "reportonDiazPertini"...
+  if (key == x) //ex. "covel"
+    var value = dict[x]; //secondo il principio var value = dict[key] "MarkCovelscase", "reportonDiazPertini"...
     const selectedParagraph = document.querySelectorAll(value); //associa l'id del paragrafo ("value") al paragrafo, selezionato = tutto salvato in una variabile
-      if ($(this).prop("checked") == true)
+      if (x.prop("checked") == true)
          selectedParagraph.style.backgroundColor = "#fff";
-      else if ($(this).prop("checked") == false)
+      else if (x.prop("checked") == false)
         selectedParagraph.style.backgroundColor = "transparent";
   // do something with "key" and "value" variables
  }
