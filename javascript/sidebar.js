73bde0a114e3e8e821966e359d5d5c0d7cb8ab13
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-03-04 10:09:27 +0100

    Update sidebar.js

--- javascript/sidebar.js
+++ javascript/sidebar.js
@@ -1,12 +1,13 @@
 
-  $('#covel').click(function(){
-        if ($(this).prop("checked") == true)
-          {
-            $('#MarkCovelscase').css({"background-color": "#fff"});
-            $('#article1').scrollTop();
-          }
-        else if ($(this).prop("checked") == false)
-         {
-          $('#MarkCovelscase').css({"background-color": "transparent"});
-         }
-    });
+
+      $('#covel').click(function(){
+            if ($(this).checked = true)
+              {
+                $('#MarkCovelscase').css({"background-color": "#fff"});
+                $('#article1').scrollTop('');
+              }
+            else if($(this).checked = false)
+             {
+              $('#MarkCovelscase').css({"background-color": "transparent"});
+             }
+        });
