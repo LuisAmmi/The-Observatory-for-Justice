0b0ccfc7ae69dd6a78949b2c6c3054a92fcbc10d
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-03-07 18:32:58 +0100

    h

--- javascript/filternavbar.js
+++ javascript/filternavbar.js
@@ -9,26 +9,49 @@ var dict = {
 "outside": "theoutsideworld",
 "next": "whathappenednext"
 };
+var linkValue = document.getElementById('stylepage');
+var hrefValue = $(linkValue).attr('href');
 //  : "2-intro", : "2-badapples", : "2-evidencebased", : "updatesandcorrections2", : "references2", : "abstract3",  : "3-intro",  : "3-methods",  : "3-results", : "3-discussion", : "3-conclusions", : "authorandarticleinfo3", : "references3"
 //cos'è this? l'elemento nel complesso? oppure l'id? per prendere solo l'id forse bisogna fare idthis = $this.getId...
 
 for (key in dict) {  // "covell", "diaz", "bolzaneto", "outside", "next"
   if (key == name) {//ex. "covel"
     var value = dict[name]; //secondo il principio var value = dict[key] "MarkCovelscase", "reportonDiazPertini"...
-  //  const selectedParagraph = document.querySelectorAll(value); //associa l'id del paragrafo ("value") al paragrafo, selezionato = tutto salvato in una variabile
-      if (x.checked == true){
-        document.getElementById(value).style.backgroundColor = "red";
-        //$('#' + value).css('background', '#fff url("imgs/70/sfondo verde.png"');
-         //document.getElementById(value).css('background', '#fff url("../imgs/70/sfondo bianco.png"');
-       }
-      else if (x.checked == false) {
-        document.getElementById(value).style.backgroundColor = transparent;
-      //  $('#' + value).css('background', 'transparent url("imgs/70/sfondo bianco.png"');
-      //  document.getElementById(value).css('background', 'transparent url("../imgs/70/sfondo bianco.png"');
+      if ((hrefValue == "css/issuedada.css") || (hrefValue == "css/issuebasic.css")){
+            if (x.checked == true){
+              document.getElementById(value).style.backgroundColor = "red";
+            //$('#' + value).css('background', '#fff url("imgs/70/sfondo verde.png"');
+             //document.getElementById(value).css('background', '#fff url("../imgs/70/sfondo bianco.png"');
+             }
+            else if (x.checked == false) {
+              document.getElementById(value).style.backgroundColor = "transparent";
+            //  $('#' + value).css('background', 'transparent url("imgs/70/sfondo bianco.png"');
+          //  document.getElementById(value).css('background', 'transparent url("../imgs/70/sfondo bianco.png"');
+            }
       }
-  //      selectedParagraph.style.backgroundColor = "transparent";
-  // do something with "key" and "value" variables
+      else if (hrefValue == "css/issue70.css"){
+            if (x.checked == true){
+             document.getElementById(value).style.border = 'red solid 0.5vh';
+
+            //  var selectedSection = document.getElementById(value);
+            //  for ($("p") in selectedSection) {
+           //      $("p").css('background-image', 'url("../imgs/70/sfondo rosa.png")');
+            //  }
+
+              //selectedSection.querySelectorAll('p').css('background-image', 'url("../imgs/70/sfondo rosa.png")'); //style.backgroundColor = "red"; //css('background-image', 'url("../imgs/70/sfondo rosa.png")');
+            //$('#' + value).css('background', '#fff url("imgs/70/sfondo verde.png"');
+             //document.getElementById(value).css('background', '#fff url("../imgs/70/sfondo bianco.png"');
+             }
+            else if (x.checked == false) {
+              document.getElementById(value).style.border = '0';
+            //  $('#' + value).css('background', 'transparent url("imgs/70/sfondo bianco.png"');
+          //  document.getElementById(value).css('background', 'transparent url("../imgs/70/sfondo bianco.png"');
+            }
+      }
    }
  }
 };
+
 // fine funzione Highlight section
+//$('#stylepage').attr('href', 'css/issuedada.css');
+//<link rel="stylesheet" href="css/issuebasic.css" id='stylepage'>
