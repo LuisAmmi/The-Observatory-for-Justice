7202d2a2197e728ff8200d15c92579a7b1e39136
Luisa Ammirati <luisa_ammirati@hotmail.it>
2022-03-10 18:38:54 +0100

    CAM

--- resultofPythonfunction.js
+++ resultofPythonfunction.js
@@ -1,6 +1,6 @@
 FOR EACH CATEGORY: ALL THE 'ABOUT'S AND THE NUM OF TIMES THEY APPEAR
 
-company [{'Independent Media Center': 2}, {'Getty': 1}, {'PacificPress': 1}, {'PubMed': 4}, {'Article': 12}, {'GoogleScholar': 118}, {'Web of Science': 3}, {'PsycINFO': 1}, {'JSTOR': 1}, {'Criminal Justice Abstracts': 1}, {'Sociological Abstracts': 1}, {'National Criminal Justice Reference Service': 1}, {'Crossref': 57}, {'Medline': 50}]
+company organization [{'Independent Media Center': 2}, {'Getty': 1}, {'PacificPress': 1}, {'PubMed': 4}, {'Article': 12}, {'GoogleScholar': 118}, {'Web of Science': 3}, {'PsycINFO': 1}, {'JSTOR': 1}, {'Criminal Justice Abstracts': 1}, {'Sociological Abstracts': 1}, {'National Criminal Justice Reference Service': 1}, {'Crossref': 57}, {'Medline': 50}]
 
 company newspaper [{'La Repubblica': 1}, {'Daily Mail': 1}, {'Nature': 2}, {'The New York Times': 1}]
 
@@ -171,3 +171,6 @@ concept [
 {'police violence': 1},
 {'misconduct': 7},
 {'abuse of power': 1}]
+
+company organization [{'Independent Media Center': 2}, {'Getty': 1}, {'PacificPress': 1}, {'PubMed': 4}, {'Article': 12}, {'GoogleScholar': 118}, {'Web of Science': 3}, {'PsycINFO': 1}, {'JSTOR': 1}, {'Criminal Justice Abstracts': 1}, {'Sociological Abstracts': 1}, {'National Criminal Justice Reference Service': 1}, {'Crossref': 57}, {'Medline': 50}]
+company newspaper [{'La Repubblica': 1}, {'Daily Mail': 1}, {'Nature': 2}, {'The New York Times': 1}]
